75a16239246a367b2bf1b09be2db19f64d93c715
Marianna <marianna.albrici@gmail.com>
2023-05-09 14:46:29 +0200

    Added exercises

--- src/exerciseOne/Main.java
+++ src/exerciseOne/Main.java
@@ -1,17 +1,40 @@
 package exerciseOne;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println();
-		
+
 		int wordNo = input.nextInt();
-		
-		Set<String> words = new HashSet<String>()
-		
+
+		Set<String> words = new HashSet<String>();
+		Set<String> duplicateWords = new HashSet<String>();
+
+		for (int i = 0; i < wordNo; i++) {
+
+			String word = input.nextLine();
+			if (!words.add(word)) {
+				duplicateWords.add(word);
+			}
+		}
+
+		for (String word : duplicateWords) {
+			System.out.println(word);
+		}
+
+		for (String word : words) {
+			System.out.print(word);
+		}
+
+		int duplicatedWords = duplicateWords.size();
+		System.out.println(duplicatedWords);
+
+		input.close();
 	}
 
 }
---
+++ src/exerciseThree/ContactsList.java
@@ -0,0 +1,44 @@
+package exerciseThree;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContactsList {
+
+	private Map<String, String> ContactsList;
+
+	public ContactsList() {
+		ContactsList = new HashMap<String, String>();
+	}
+
+	public void addContact(String name, String number) {
+		ContactsList.put(name, number);
+	}
+
+	public void deleteContact(String name) {
+		ContactsList.remove(name);
+	}
+
+	public String searchContactByNumber(String number) {
+		for (String name : ContactsList.keySet()) {
+			if (ContactsList.get(name).equals(number)) {
+				return name;
+			}
+		}
+		return null;
+	}
+
+	public String searchContactByName(String name) {
+		return ContactsList.get(name);
+	}
+
+	public void printContactsList() {
+		System.out.println("Here's your contacts list: ");
+		for (String name : ContactsList.keySet()) {
+			String number = ContactsList.get(name);
+			System.out.println(name + ": " + number);
+
+		}
+
+	}
+}
