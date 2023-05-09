51950950edaf9f57db24d8c681b89df934ccda9b
Marianna <marianna.albrici@gmail.com>
2023-05-09 14:04:22 +0200

    Create Main.java

---
+++ src/exerciseOne/Main.java
@@ -0,0 +1,17 @@
+package exerciseOne;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println();
+		
+		int wordNo = input.nextInt();
+		
+		Set<String> words = new HashSet<String>()
+		
+	}
+
+}
